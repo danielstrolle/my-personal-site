8974e684b05b90c038e6063f5213c4d49f154c39
Daniel Strolle <danielstrolle@gmail.com>
2018-08-29 14:14:11 -0500

    Create a desktop navbar component that shows all 3 links for navigation thru the page...(need to center)

---
+++ src/components/DesktopNav.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-toolbar
+    app
+  >
+    <v-toolbar-items>
+      <v-btn flat>Work</v-btn>
+      <v-btn flat>Projects</v-btn>
+      <v-btn flat>Personal</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
