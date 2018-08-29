435546176851c254b4c408b0b6aa72d575bbf57d
Daniel Strolle <danielstrolle@gmail.com>
2018-08-29 14:14:57 -0500

    Create a mobile navbar component that shows a collapsed menu containing the 3 links for site navigation...(need to center)

---
+++ src/components/MobileNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-toolbar
+    app
+  >
+    <v-toolbar-items>
+      <v-menu offset-y>
+        <v-btn
+          flat
+          slot="activator"
+        >
+          <v-icon>child_care</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-tile
+            v-for="(item, index) in items"
+            :key="index"
+            @click="">
+            <v-list-tile-title>{{item.title}}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {title: 'Work'},
+          {title: 'Projects'},
+          {title: 'Personal'}
+        ]
+      }
+    }
+  }
+</script>
