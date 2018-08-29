106c3979028f322567b9938da0dfad94eb4b7daf
Daniel Strolle <danielstrolle@gmail.com>
2018-08-29 14:15:56 -0500

    create 2 parallax components with different sizes based on if on mobile or not

--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -1,16 +1,16 @@
 <template>
   <section>
     <v-flex xs12 text-xs-center>
-      <v-parallax src="../../static/img/San-Antonio-Skyline.jpg">
-        <v-layout
-          align-center
-          column
-          justify-center
-        >
-          <h1 class="display-4" style="font-family: 'Ubuntu', sans-serif;">Daniel Strolle</h1>
-          <h4 class="display-1">Full Stack Developer</h4>
-        </v-layout>
-      </v-parallax>
+      <mq-layout mq="desktop">
+        <v-parallax src="../../static/img/San-Antonio-Skyline.jpg">
+        </v-parallax>
+      </mq-layout>
+      <mq-layout mq="mobile">
+        <v-parallax src="../../static/img/San-Antonio-Skyline-Mobile.jpg" height="300">
+        </v-parallax>
+      </mq-layout>
+      <img src="../../static/img/TestImg.jpg" alt="ProfilePic"
+           style="border-radius: 50%; position: relative; top: -77px; margin-bottom: -77px; border: 2px solid white; box-shadow: inset 0 1.5px 3px 0 rgba(0,0,0,.15), 0 1.5px 3px 0 rgba(0,0,0,.15);">
     </v-flex>
   </section>
 </template>
