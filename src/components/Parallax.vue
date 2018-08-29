08c5940bf67fce2991c67021f8cfe5fd52dfc4eb
Daniel Strolle <danielstrolle@gmail.com>
2018-08-29 14:18:19 -0500

    add alt tags for images

--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -2,14 +2,14 @@
   <section>
     <v-flex xs12 text-xs-center>
       <mq-layout mq="desktop">
-        <v-parallax src="../../static/img/San-Antonio-Skyline.jpg">
+        <v-parallax src="../../static/img/San-Antonio-Skyline.jpg" alt="San Antonio Skyline">
         </v-parallax>
       </mq-layout>
       <mq-layout mq="mobile">
-        <v-parallax src="../../static/img/San-Antonio-Skyline-Mobile.jpg" height="300">
+        <v-parallax src="../../static/img/San-Antonio-Skyline-Mobile.jpg" alt="San Antonio Skyline" height="300">
         </v-parallax>
       </mq-layout>
-      <img src="../../static/img/TestImg.jpg" alt="ProfilePic"
+      <img src="../../static/img/TestImg.jpg" alt="Profile Pic"
            style="border-radius: 50%; position: relative; top: -77px; margin-bottom: -77px; border: 2px solid white; box-shadow: inset 0 1.5px 3px 0 rgba(0,0,0,.15), 0 1.5px 3px 0 rgba(0,0,0,.15);">
     </v-flex>
   </section>
