6a3fca38a94fe89c595dd9323076e5b24dc18c12
Daniel Strolle <danielstrolle@gmail.com>
2018-08-30 15:22:42 -0500

    move data to parent component and pass it down to child component, catch as prop and parse through it

--- src/components/PersonalActivity.vue
+++ src/components/PersonalActivity.vue
@@ -21,28 +21,6 @@
 
 <script>
   export default {
-    data() {
-      return {
-        activities: [
-          {
-            title: 'Fitness',
-            icon: 'fitness_center',
-            list: ['Basketball', 'Weightlifting', 'Sand-Volleyball', 'Football', 'Walking'],
-            desc: 'Whether it be through competition, or just self-betterment, fitness gives me a way to push my body and test myself, constantly setting and meeting new goals. The temporary release from the stresses of life, also works to keep my mind fresh and productive. These are what I enjoy: '
-          },
-          {
-            title: 'Cooking',
-            icon: 'restaurant_menu',
-            list: ['Baking', 'Stove-Top', 'Slow Cooker', 'Grilling'],
-            desc: 'Although cleaning isn\'t the most fun, I love to cook! Cooking with others and trying new recipes is a fun social activity that makes me a more patient person. Getting to eat my own food is of course the best part! Here are my most common methods: '
-          },
-          {
-            title: 'Gaming',
-            icon: 'headset_mic',
-            list: ['Counter-Strike: Global Offensive', 'League of Legends'],
-            desc: 'PC Master Race of course, the competitive environment these games present allow me to have fun competing and testing my skill against others online. Sometimes I get a little too loud, what can I say, im passionate, but its always a good time. Here are the games I play: '
-          }]
-      }
-    }
+    props: ['activities']
   }
 </script>
