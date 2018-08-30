fb8eb2386ae8f4d2d5a601758e2df79ceb7f90f4
Daniel Strolle <danielstrolle@gmail.com>
2018-08-30 12:52:42 -0500

    Create work component listing previous/current job experience(s)
    present experience in a card

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,34 +1,43 @@
 <template>
-  <v-flex xs12 text-xs-center style="background-color: lightgray">
-    <v-layout row>
-      <v-flex xs12 mt-5>
-        <v-flex xs4 offset-xs4>
-          <h4 class="display-2">Work</h4>
-        </v-flex>
+  <section>
+    <v-layout row class="text-xs-center mt-3">
+      <v-flex xs10 offset-xs1>
+        <h1 text-xs-center class="display-1">Work</h1>
       </v-flex>
     </v-layout>
-    <v-layout row>
-      <v-flex xs12 mt-3>
-        <v-flex xs6 offset-xs3>
-          <p style="font-size: 1.2em">
-            My experiences thus far have allowed me to continue to pickup new pieces, understand overarching trends and themes, and put the puzzle together. The right opportunity is always right around the corner.
-          </p>
-        </v-flex>
+    <v-layout row class="mt-3">
+      <v-flex xs12>
+        <hr>
       </v-flex>
     </v-layout>
-    <v-layout row>
-      <v-flex xs4 offset-xs4 mt-3>
-        <h4 class="display-2">Experience</h4>
+    <v-layout row class="mt-3" v-for="(job, index) in jobs" :key="index">
+      <v-flex xs12>
+        <v-card>
+          <v-layout row>
+              <v-card-text class="title">{{job.company}}</v-card-text>
+          </v-layout>
+          <v-layout row>
+            <v-card-text class="subheading">{{job.title}} | {{job.dates}}</v-card-text>
+          </v-layout>
+          <v-layout row>
+            <v-card-text>{{job.desc}}</v-card-text>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
-    <WorkExperience></WorkExperience>
-  </v-flex>
+  </section>
 </template>
 
 <script>
   import WorkExperience from './WorkExperience.vue'
 
   export default {
-    components: {WorkExperience}
+    components: {WorkExperience},
+    data () {
+      return {
+        jobs: [{company: 'Spill.ai', title: 'Junior Developer', dates: 'July 2018 - September 2018', desc: 'Created a mobile responsive User Interface using Vue.js, integrating many UI components on a best use-case basis. These include implementing Auto-completes, routing, date-pickers, search filters, and pagination. Also participated in design meetings about the App layout, and stand ups about the weeks progress. Worked on the API, using C# and .NET Core, creating methods to retrieve the appropriate information for all the endpoints, including meta-data. Additionally wrote unit tests to document that they were working as intended.'},
+          {company: 'Intech Southwest Services', title: 'Warehouse Technician', dates: 'August 2016 - July 2017', desc: 'Performed QA and testing on hardware components prior to delivery and deployment for production. Developed a system that maintained hardware inventory in an effort to automate purchase order fulfillment requirements.'}]
+      }
+    }
   }
 </script>
