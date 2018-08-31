ccab4b4577a7a13b2e62e50bf3492f5f3a090f1f
Daniel Strolle <danielstrolle@gmail.com>
2018-08-31 11:56:18 -0500

    using mq create a desktop view that is more easily read/user friendly
    reformat bio to show info under picture (now left aligned) and the certs on the right

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,33 +1,51 @@
 <template>
   <section>
-    <v-layout row>
-      <v-flex xs10 offset-xs1>
-        <h1 text-xs-center class="display-2">Daniel Strolle</h1>
-        <h2 text-xs-center class="Title">Junior Developer @ Spill.ai</h2>
-      </v-flex>
-    </v-layout>
-    <v-layout row mt-3>
-      <v-flex xs12>
-        <h4 class="title text-xs-center">Certifications</h4>
-        <br>
-        <hr>
-        <ul class="mt-3">
-          <li>OCA Java SE 8 Programmer</li>
-          <li>Codeup - Certificate of Completion</li>
-        </ul>
-      </v-flex>
-    </v-layout>
-    <v-layout row mt-3>
-      <v-flex xs12>
-        <v-card>
-          <v-container fluid grid-list-lg>
-            <v-card-text>
-              My experiences thus far have allowed me to continue to pickup new pieces, understand overarching trends and themes, and put the puzzle together. The right opportunity is always right around the corner!
-            </v-card-text>
-          </v-container>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <mq-layout mq="mobile">
+      <v-layout row>
+        <v-flex xs10 offset-xs1>
+          <h1 class="display-2 text-xs-center">Daniel Strolle</h1>
+          <h2 class="Title text-xs-center">Junior Developer @ Spill.ai</h2>
+        </v-flex>
+      </v-layout>
+      <v-layout row mt-3>
+        <v-flex xs12>
+          <h4 class="title text-xs-center">Certifications</h4>
+          <br>
+          <hr>
+          <ul class="mt-3">
+            <li>OCA Java SE 8 Programmer</li>
+            <li>Codeup - Certificate of Completion</li>
+          </ul>
+        </v-flex>
+      </v-layout>
+      <v-layout row mt-3>
+        <v-flex xs12>
+          <v-card>
+            <v-container fluid grid-list-lg>
+              <v-card-text>
+                My experiences thus far have allowed me to continue to pickup new pieces, understand overarching trends and themes, and put the puzzle together. The right opportunity is always right around the corner!
+              </v-card-text>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </mq-layout>
+    <mq-layout mq="desktop">
+      <v-layout row>
+        <v-flex sm4 offset-sm2>
+          <h1 class="display-2">Daniel Strolle</h1>
+          <h2 class="Title">Junior Developer @ Spill.ai</h2>
+        </v-flex>
+        <v-flex sm2 offset-sm2>
+          <h2 class="Title text-xs-center">Certifications</h2>
+          <hr>
+          <ul class="mt-3">
+            <li>OCA Java SE 8 Programmer</li>
+            <li>Codeup - Certificate of Completion</li>
+          </ul>
+        </v-flex>
+      </v-layout>
+    </mq-layout>
   </section>
 </template>
 
